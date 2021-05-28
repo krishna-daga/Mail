b2dba9dbe3dd01d0ac10d5cbd5f350576c8ea26c
Krishna daga <krishna20299@gmail.com>
2021-05-28 19:26:30 +0530

    starting

---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').onsubmit=()=>{
+
+    fetch('/emails', {
+      method: 'POST',
+      // get form inputs
+      body: JSON.stringify({
+          recipients: document.querySelector('#compose-recipients').value,
+          subject: document.querySelector('#compose-subject').value,
+          body: document.querySelector('#compose-body').value
+          
+      })
+      
+    })
+    .then(response => response.json())
+    .then(result => {
+      if (result["error"]) {
+        document.querySelector('#message').innerHTML = result["error"];
+        document.querySelector('.alert').style.display = 'block';
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+      }
+      else {
+        document.querySelector('.alert').style.display = 'none';
+        load_mailbox('sent')
+    }
+  });
+  return false;
+};
+  
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+function compose_email() {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+  
+}
+  
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+// function send_email() 
